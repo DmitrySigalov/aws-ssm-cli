0af8f7117954fcf10160358b5e0d641f48d48adb
Dmitry Sigalov <dmitry.s@lsports.eu>
2023-08-30 10:58:29 +0300

    add project files

--- src/Ssm.Aws.Cli/Program.cs
+++ src/Ssm.Aws.Cli/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-Console.WriteLine("Hello, World!");
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (s, e) =>
+{
+    cts.Cancel();
+    e.Cancel = true;
+};
+
+var configuration = new ConfigurationBuilder()
+    //.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", false)
+    .AddEnvironmentVariables()
+    .Build();
+
+var services = new ServiceCollection();
+
+services.AddSingleton<IConfiguration>(configuration);
+
+var provider = services.BuildServiceProvider();
+var logger = provider.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    var relevantArgs = args.Where(arg => arg != "--debug").ToArray();
+
+    throw new Exception("Test exception");
+}
+catch (Exception e)
+{
+    logger.LogError(e, "An error has occurred");
+}

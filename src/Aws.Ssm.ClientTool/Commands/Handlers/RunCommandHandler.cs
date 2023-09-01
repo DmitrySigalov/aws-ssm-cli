e7a441a64190e780cfbc008454395d94bd46e81e
Dmitry Sigalov <dmitry.s@lsports.eu>
2023-09-01 16:59:08 +0300

    profile stub

--- src/Aws.Ssm.ClientTool/Commands/Handlers/RunCommandHandler.cs
+++ src/Aws.Ssm.ClientTool/Commands/Handlers/RunCommandHandler.cs
@@ -1,7 +1,16 @@
+using Aws.Ssm.ClientTool.Profiles;
+
 namespace Aws.Ssm.ClientTool.Commands.Handlers;
 
 public class RunCommandHandler : ICommandHandler
 {
+    private readonly ProfilesRepository _profilesRepository;
+
+    public RunCommandHandler()
+    {
+        
+    }
+    
     public string Name => "run";
     
     public Task Handle(CancellationToken cancellationToken)

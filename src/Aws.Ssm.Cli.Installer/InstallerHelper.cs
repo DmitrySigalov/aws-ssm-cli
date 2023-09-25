fa6f4eeaeafad949f658ef9872f3405c0f95872f
Dmitry Sigalov <dmitry.s@lsports.eu>
2023-09-25 07:23:36 +0300

    application service path fix

--- src/Aws.Ssm.Cli.Installer/InstallerHelper.cs
+++ src/Aws.Ssm.Cli.Installer/InstallerHelper.cs
@@ -1,16 +1,53 @@
+using System.Runtime.InteropServices;
 using Microsoft.Extensions.Configuration;
 
 namespace Aws.Ssm.Cli.Installer;
 
 public static class InstallerHelper
 {
+    private static string ClientToolName => "aws-ssm-cli";
+    
     public static string GetBuildWorkingDirectory(IConfiguration configuration)
     {
-        return "../../../../../";
+        var path = configuration["buildWorkingDirectory"];
+        if (!string.IsNullOrEmpty(path))
+        {
+            return path;
+        }
+        
+        return "../";
     }
     
-    public static string GetBuildOutputDirectory(IConfiguration configuration)
+    public static string GetAppHomeDirectory(IConfiguration configuration)
     {
-        return "app";
+        var path = configuration["appHomePath"];
+        if (!string.IsNullOrEmpty(path))
+        {
+            if (Path.IsPathRooted(path))
+            {
+                return path;
+            }
+
+            path = Path.Combine(
+                GetBuildWorkingDirectory(configuration),
+                path);
+
+            path = Path.GetFullPath(path);
+
+            return path;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var windowsPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            return Path.Combine(windowsPath, ClientToolName);
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            return $"/usr/local/share/{ClientToolName}";
+        }
+
+        throw new NotSupportedException($"Not supported {RuntimeInformation.RuntimeIdentifier}");
     }
 }
--- src/Aws.Ssm.Cli.Installer/Program.cs
+++ src/Aws.Ssm.Cli.Installer/Program.cs
@@ -1,24 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using Aws.Ssm.Cli.Installer;
 using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Installing aws-ssm-cli...\n");
+Console.WriteLine("Installing aws-ssm-cli...");
+
+if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows) &&
+    !RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+{
+    throw new NotSupportedException($"Not supported {RuntimeInformation.RuntimeIdentifier}");
+}
+
 
 var configuration = new ConfigurationBuilder()
     .AddCommandLine(args)
     .Build();
 
-var workingDirectory = InstallerHelper.GetBuildWorkingDirectory(configuration);
-var output = InstallerHelper.GetBuildOutputDirectory(configuration);
+var buildWorkingDirectory = InstallerHelper.GetBuildWorkingDirectory(configuration);
+var appHomePath = InstallerHelper.GetAppHomeDirectory(configuration);
+
+Console.WriteLine("Installing...");
 
 var process = Process.Start(new ProcessStartInfo
 {
     FileName = "dotnet",
-    WorkingDirectory = workingDirectory,
+    WorkingDirectory = buildWorkingDirectory,
     Arguments =
-        $"publish src/Aws.Ssm.Cli/Aws.Ssm.Cli.csproj --output {output} --source https://api.nuget.org/v3/index.json --configuration Release --verbosity quiet /property:WarningLevel=0"
+        $"publish src/Aws.Ssm.Cli/Aws.Ssm.Cli.csproj --output {appHomePath} --source https://api.nuget.org/v3/index.json --configuration Release --verbosity quiet /property:WarningLevel=0"
 });
 
 await process!.WaitForExitAsync();
+
+Console.WriteLine("Adding app to machine path...");
+

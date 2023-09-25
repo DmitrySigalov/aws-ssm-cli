cc217a2900b2ada1a1aaf351b48ca9f4de846d2f
Dmitry Sigalov <dmitry.s@lsports.eu>
2023-09-25 09:04:41 +0300

    fix ascli argument

--- src/Aws.Ssm.Cli/ascli.bat
+++ src/Aws.Ssm.Cli/ascli.bat
@@ -3,8 +3,8 @@ if "%OS%" == "Windows_NT" setlocal
 
 setlocal enabledelayedexpansion
 
-set OKTA_AWS_CLI_HOME="%~dp0"
+set AWS_SSM_CLI_HOME="%~dp0"
 
 setlocal DISABLEDELAYEDEXPANSION
 
-%OKTA_AWS_CLI_HOME%okta-aws-cli.exe %*
+%AWS_SSM_CLI_HOME%aws-ssm-cli.exe %*
